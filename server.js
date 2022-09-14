f3eae8ba9312286b3181932f50284185a6fa60c1
Nikhil Arora <2020mt93507@wilp.bits-pilani.ac.in>
2022-09-15 04:20:34 +0530

    DB setup for Employee connect

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 
 const app = express();
 
+// DB connection
+connectDB();
+
+
 app.get('/', (req,res)=> res.send('API running'))
+
+
 // if port is there in env file then will pick that otherwise 5000
 const PORT = process.env.PORT || 5000; 
 
