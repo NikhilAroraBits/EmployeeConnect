90ad4449ad47fc6af437decb65358ed1419f827d
Nikhil Arora <2020mt93507@wilp.bits-pilani.ac.in>
2022-09-18 03:04:52 +0530

    get current profile user changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,29 @@
 
 const express = require('express');
-
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route        GET api/profile
-//  @desc        Profile route
-//  @access      Public
+// @route        GET api/profile/me
+//  @desc        Get current users profile
+//  @access      Private // because token would be required to see own profile
 
-router.get("/", (req,res)=> res.send('Profile route'));
+router.get('/me', auth, async (req, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: req.user.id
+      }).populate('user', ['name', 'avatar']); // after finding user name it populates name and avatar
+  
+      if (!profile) {
+        return res.status(400).json({ msg: 'There is no profile for this user' });
+      }
+  
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
 
 module.exports=router;
